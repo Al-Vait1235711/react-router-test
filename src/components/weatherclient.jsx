4b51f419d621b91789dc44cee7b60b3800fc82df
Al V <alv.vait@outlook.com>
2025-03-15 14:37:21 +0000

    Update weatherclient.jsx

--- src/components/weatherclient.jsx
+++ src/components/weatherclient.jsx
@@ -49,7 +49,7 @@ export default function WeatherApiClient(props) {
 
     useEffect(() => {
 
-        const delay = () => new Promise(resolve => setTimeout(resolve, 2000))
+        const delay = () => new Promise(resolve => setTimeout(resolve, 0.1))
         const r = async () => {
             if (((props.city) && (props.lat) && (props.lng))) {
                 await getData();
