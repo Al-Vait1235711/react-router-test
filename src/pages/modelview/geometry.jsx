d081601ef1ca97cf74193db788426a3e325cb2f3
A Vaitkus <alv.vait@outlook.com>
2025-04-09 22:18:01 +0100

    ..

---
+++ src/pages/modelview/geometry.jsx
@@ -0,0 +1,143 @@
+import { LineGeometry } from 'three/addons/lines/LineGeometry.js';
+import {  LineMaterial} from 'three-stdlib';
+import { Color } from 'three';
+
+
+/**
+ * Extends LineGeometry
+ */
+class CircleGeometry extends LineGeometry {
+    constructor() {
+        super();
+        this.verteces = [];
+    }
+
+    /**
+     *
+     * @param {*} centre: array:[x,y,z]
+            * @param {*} radius: number
+            * @param {*} segments: number
+            */
+    setFromCentreRadius(centre, radius, segments) {
+        const segment = Math.PI * 2 / segments
+        for (let i = 0; i < segments + 1; i++) {
+            var x = null;
+            var y = null;
+            var z = null;
+            x = centre[0] + (radius * Math.cos(segment * i));
+            y = centre[1] + (radius * Math.sin(segment * i));
+            z = centre[2];
+            this.verteces.push(x, y, z)
+
+        }
+        const geometry = this.setPositions(this.verteces)
+        console.log(geometry)
+        return geometry
+    }
+
+}
+
+/**
+ * Extends LineGeometry
+ */
+
+
+class PointGeometry {
+    constructor() {
+        this.verteces = [];
+        this.radius = 0;
+        this.centre = [];
+        this.rotate = 0
+        this.pointtype = null;
+    }
+    /**
+     *
+     * @param {*} centre: array:[x,y,z]
+            * @param {*} radius: number
+            * @param {*} segments: number
+            * @param {*} rotate: number => degrees 0-90  default: 0
+            */
+    setFromCentreRadius(centre, radius, segments, rotate, pointtype) {
+        this.pointtype = pointtype;
+        this.centre = centre
+        this.radius = radius
+        this.rotate = rotate
+        const segment = Math.PI * 2 / segments
+        for (let i = 0; i < segments + 1; i++) {
+            var x = null;
+            var y = null;
+            var z = null;
+            x = centre[0] + (radius * Math.cos(segment * i));
+            y = centre[1] + (radius * Math.sin(segment * i));
+            z = centre[2];
+            this.verteces.push(x, y, z)
+
+        }
+        const geometry = new LineGeometry().setPositions(this.verteces)
+        const _ll = this.orthoCross()
+        // const testgeom = mergeBufferGeometries([_ll, geometry], false)
+        return [geometry, _ll[0], _ll[1]]
+    }
+
+    orthoCross() {
+        const vert = []
+        const _angle = Math.PI * 2 / 4 //need 4 points for two lines, each line two points
+        const _stangle = this.rotate * Math.PI / 180
+        const _length = this.radius * 1.2 // line extends 10% pass the radius
+        for (let i = 0; i < 2; i++) {
+            for (let n = 0; n < 3; n += 2) {
+                var x = this.centre[0] + (_length * Math.cos(_stangle + _angle * (i + n)));
+                var y = this.centre[1] + (_length * Math.sin(_stangle + _angle * (i + n)));
+                var z = this.centre[2];
+                vert.push(x, y, z)
+            }
+        }
+        const geom1 = new LineGeometry().setPositions(vert.slice(0, 6))
+        const geom2 = new LineGeometry().setPositions(vert.slice(6, 12))
+        return [geom1, geom2]
+    }
+
+}
+
+
+function CustPoint(props) {
+
+    var radius = 0.02
+    var segments = 40
+    var rotate = 0
+    var position = [0,0,0]
+
+    if(!props.position && !props.test){
+        return
+    } else if(props.position){
+        position = props.position
+    }
+    if(props.radius){
+        radius = props.radius
+    } 
+    if(props.segments){
+        segments= props.segments
+    } 
+    if(props.rotate){
+        rotate = props.rotate
+    } 
+
+
+    const geometry = new PointGeometry().setFromCentreRadius(position, radius, segments, rotate);
+    const material = new LineMaterial();
+    // const material = new THREE.MeshBasicMaterial()
+    material.color = new Color('#ff0000');
+    material.linewidth = 0.002;
+    return (<group>
+        <mesh geometry={geometry[0]} material={material} ></mesh>
+        <mesh geometry={geometry[1]} material={material} ></mesh>
+        <mesh geometry={geometry[2]} material={material} ></mesh>
+    </group>)
+
+}
+
+
+
+
+
+export { PointGeometry, CircleGeometry, CustPoint }
--- src/pages/modelview/viewport.jsx
+++ src/pages/modelview/viewport.jsx
@@ -1,12 +1,11 @@
 import { Container, Row, Col } from "react-bootstrap";
 import './modelviewp.css';
 import * as THREE from 'three';
-import { Line2 } from 'three/addons/lines/Line2.js';
 import { LineGeometry } from 'three/addons/lines/LineGeometry.js';
 import { Canvas, extend, useThree } from '@react-three/fiber';
-import { OrbitControls, LineMaterial } from 'three-stdlib';
-extend({ OrbitControls });
-
+import { OrbitControls, LineMaterial, mergeBufferGeometries } from 'three-stdlib';
+import { PointGeometry, CircleGeometry, CustPoint } from "./geometry";
+extend({ OrbitControls, });
 import data from '../../test/test1.json';
 
 
@@ -16,34 +15,30 @@ import data from '../../test/test1.json';
 
 export default function ModelViewPort() {
 
+
     return (
         <>
             <Container fluid>
                 <Row >
                     <Col sm={12} className="weather-div title-holder">
-                        <div className="disp-inline">Simple use of Weather API</div>
-                        <div className="disp-inline api-ref">(open-meteo)</div>
+                        <div className="disp-inline">Three-JS paractice</div>
+                        <div className="disp-inline api-ref">(Some items drawn in canvas)</div>
                     </Col>
                 </Row>
-                <Row>
-                    <div className="viewport-holder">
-                        <Col sm={12}>
-                            <Canvas>
-                                <ambientLight intensity={0.1} />
-                                <directionalLight position={[0, 0, 5]} color="white" />
-                                <directionalLight position={[-1000, -1000, -1000]} color="white" />
-                                <directionalLight position={[1000, 1000, 1000]} color="white" />
-                                <ControlsOrbit enableRotate={false}/>
+            </Container>
+            <Container fluid style={{ padding: '0px' }}>
 
-
-
-
-                                <CLine2 />
-                            </Canvas>
-
-                        </Col>
-                    </div>
-                </Row>
+                <div className="viewport-holder">
+                    <Canvas orthographic camera={{ camera: CustCamera, zoom: 10, position: [0, 0, 500] }}>
+                        <ambientLight intensity={0.1} />
+                        <directionalLight position={[0, 0, 5]} color="white" />
+                        <directionalLight position={[-1000, -1000, -1000]} color="white" />
+                        <directionalLight position={[1000, 1000, 1000]} color="white" />
+                        <ControlsOrbit enableRotate={true} />
+                        <CLine2 color={'magenta'} />
+                        <CustPoint test/>
+                    </Canvas>
+                </div>
             </Container>
         </>
     )
@@ -63,25 +58,29 @@ function DrawItems(props) {
 
 function CLine2(props) {
 
-
-    const testLine = [1, 1, 0, 10, 10, 0]
-    const vec = new THREE.Vector3()
-    const geometry = new LineGeometry().setPositions(testLine)
-    const material = new LineMaterial()
-    material.linewidth= 0.001,
-    material.color = new THREE.Color('#ff0000')
-
+    const testLine = [-1, -1, 0, 2, 2, 0];
+    const geometry = new LineGeometry().setPositions(testLine);
+    const material = new LineMaterial();
+    if (!props.linewidth) {
+        material.linewidth = 0.002;
+    } else { material.linewidth = props.linewidth * 0.001; };
+    if (!props.color) {
+        material.color = new THREE.Color('#ff0000');
+    } else { material.color = new THREE.Color(props.color); };
     return (
-
-        <mesh geometry={geometry} material={material}>
-        </mesh>
-
-    )
+        <mesh geometry={geometry} material={material}></mesh>)
 }
 
 
-
 function ControlsOrbit(props) {
-    const { camera, gl: { domElement }, } = useThree();    
+    const { camera, gl: { domElement }, } = useThree();
     return (<orbitControls {...props} args={[camera, domElement]} />)
+}
+
+const CustCamera = (props) => {
+
+    const width = 100
+    const height = 75
+    const mycamera = new THREE.OrthographicCamera(width / -2, width / 2, height / 2, height / -2, 0.1, 500)
+    return mycamera
 }
